99aae99f1e02bb9c6ec35a18e08077c0d651abbb
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-06-25 16:14:31 -0500

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/system';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position='static'>
+      <Container maxWidth='xs'>
+        <Toolbar>
+          <Typography variant='h5' component='div' sx={{ flexGrow: 1 }}>
+            Mortgage Calculator
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
-
 export default Navbar;
