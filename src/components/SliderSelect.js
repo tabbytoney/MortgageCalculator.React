db02d1c5fb4801fea14d03fb779709054e2feb9b
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-06-25 15:59:21 -0500

    Add component folders and files, component templates

---
+++ src/components/SliderSelect.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SliderSelect = () => {
+  return <div>Slider Select</div>;
+};
+
+export default SliderSelect;
---
+++ src/components/TenureSelect.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TenureSelect = () => {
+  return <div>Tenure Select</div>;
+};
+
+export default TenureSelect;
