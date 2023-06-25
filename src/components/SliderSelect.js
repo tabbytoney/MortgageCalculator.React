35b57497724fac2f03a84bcd46f04527733f5146
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-06-25 17:27:29 -0500

    add slider component props

--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { SliderComponent } from './reused/SliderComponent';
 
 const SliderSelect = () => {
-  return <SliderComponent />;
+  return (
+    <>
+      <SliderComponent
+        min={0}
+        max={50}
+        defaultValue={20}
+        step={5}
+        onChange={(e, value) => console.log(value)}
+      />
+      {/* <SliderComponent min={100} max={200} defaultValue={150} />
+      <SliderComponent min={0} max={10} defaultValue={3} /> */}
+    </>
+  );
 };
 
 export default SliderSelect;
