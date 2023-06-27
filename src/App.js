afbe2fcfb193c605ab412475a8eb3a27d56ff44f
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-06-26 20:52:25 -0500

    Added state, max and min, calculations for slider numbers

--- src/App.js
+++ src/App.js
@@ -4,15 +4,35 @@ import ResultGraph from './components/ResultGraph';
 import SliderSelect from './components/SliderSelect';
 import TenureDropdown from './components/TenureDropdown';
 import { Grid } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  // const [homeValue, setHomeValue] = useState(0);
+  // const [downPayment, setDownPayment] = useState(0);
+  // const [loanAmount, setLoanAmount] = useState(0);
+  // const [tenure, setTenure] = useState(0);
+  // const [interestRate, setInterestRate] = useState(0);
+  // const [monthlyPayment, setMonthlyPayment] = useState(0);
+  // const [totalPayment, setTotalPayment] = useState(0);
+  // const [totalInterest, setTotalInterest] = useState(0);
+
+  // TODO: add monthly payment, total payment, total interest
+
+  const [data, setData] = useState({
+    homeValue: 150000,
+    downPayment: 150000 * 0.2,
+    loanAmount: 150000 * 0.8,
+    tenure: 30,
+    interestRate: 5,
+  });
+
   return (
     <div className='App'>
       <Navbar />
       <Container maxWidth='xl' sx={{ mt: 4 }}>
         <Grid container spacing={5}>
           <Grid item xs={12} md={6}>
-            <SliderSelect />
+            <SliderSelect data={data} setData={setData} />
             <TenureDropdown />
           </Grid>
           <Grid item xs={12} md={6}>
--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -1,38 +1,70 @@
 import React from 'react';
 import { SliderComponent } from './reused/SliderComponent';
 
-const SliderSelect = () => {
+const SliderSelect = ({ data, setData }) => {
+  const bankLimit = 600000;
   return (
     <>
+      {/* Add regex for commas in dollar amounts */}
       <SliderComponent
         label='Home Value'
-        min={0}
-        max={250}
-        defaultValue={20}
-        step={5}
-        onChange={(e, value) => console.log(value)}
+        min={150000}
+        max={bankLimit}
+        defaultValue={data.homeValue}
+        value={data.homeValue}
+        step={25000}
+        onChange={(e, value) =>
+          setData({
+            ...data,
+            downPayment: value * 0.2,
+            loanAmount: value * 0.8,
+            homeValue: value,
+          })
+        }
         unit='$'
-        amount={3000}
+        amount={data.homeValue}
       />
       <SliderComponent
         label='Down Payment'
         min={0}
-        max={150}
-        defaultValue={50}
-        step={5}
-        onChange={(e, value) => console.log(value)}
+        max={data.homeValue}
+        defaultValue={data.downPayment}
+        step={10000}
+        onChange={(e, value) =>
+          setData({
+            ...data,
+            loanAmount: data.homeValue - value,
+            downPayment: value,
+          })
+        }
         unit='$'
-        amount={500}
+        amount={data.downPayment}
       />
       <SliderComponent
         label='Loan Amount'
         min={0}
-        max={1250}
-        defaultValue={20}
-        step={5}
-        onChange={(e, value) => console.log(value)}
+        max={data.homeValue}
+        defaultValue={data.loanAmount}
+        step={25000}
+        onChange={(e, value) =>
+          setData({
+            ...data,
+            downPayment: data.homeValue - value,
+            loanAmount: value,
+          })
+        }
         unit='$'
-        amount={1250}
+        amount={data.loanAmount}
+      />
+      <SliderComponent
+        label='Interest Rate'
+        min={2}
+        max={12}
+        defaultValue={data.interestRate}
+        step={2}
+        onChange={(e, value) => setData({ ...data, interestRate: value })}
+        unit='%'
+        amount={data.interestRate}
       />
     </>
   );
